0645ea48c8bf70f7286d2c4e0c772c95e7e380d7
Shivam-Samant <shivamsamant000@gmail.com>
2024-06-06 14:23:26 +0530

    feat(service): created report service and implement a method to retrieve the class progress report

---
+++ src/services/ReportService.py
@@ -0,0 +1,21 @@
+import requests
+
+
+class ReportService:
+    def __init__(self, headers, params=None, base_url=None):
+        self.headers = headers
+        self.base_url = base_url or "http://lms.academically.com/nuSource/api/v1/reports"
+        self.params = params
+
+
+    def get_class_progress_report(self, class_id, page=1, per_page=2000):
+        url = f"{self.base_url}/classprogress"
+        default_params = {"class_id": class_id, "page": page, "per_page": per_page}
+        params = self.params or default_params
+
+        response = requests.get(url, params=params, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception("Failed to fetch report data.")
