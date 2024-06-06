8f9a8e7a0e013c408f4a95596b4c2e41af934aae
Shivam-Samant <shivamsamant000@gmail.com>
2024-06-06 14:22:20 +0530

    feat(constant): created constant file and added subject data, headers and wkhtmltopdf lib path

---
+++ src/utils/constants.py
@@ -0,0 +1,15 @@
+SUBJECT_DATA = {
+    "kaps": (238659, 74556),
+    "medicos": (262859, 89822),
+    "adc": (272073, 98161),
+    "nclex": (280097, 105398),
+    "usmle": (262232, 91181),
+    "apc": (271165, 97775),
+    "ocanz": (285909, 113240),
+    "psi": (286125, 113417),
+    "sple": (286126, 113418),
+}
+
+LMS_API_HEADERS = {"apiKey": "e63cb1851964c2aea0c3a1836cdd4b98", "ORGID": "5735"}
+
+WKHTMLTOPDF_PATH = "/usr/local/bin/wkhtmltopdf"
