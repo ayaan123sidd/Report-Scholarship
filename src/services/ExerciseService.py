6804fd420073a3f4c7e5c5f5da35ebf9ed796994
Shivam-Samant <shivamsamant000@gmail.com>
2024-06-06 14:54:12 +0530

    minor changes

--- src/services/ExerciseService.py
+++ src/services/ExerciseService.py
@@ -12,18 +12,20 @@ class ExerciseService:
         url = f"{self.base_url}/{test_id}/attemptlist?class_id={class_id}&user_id={student_id}&is_quiz=false"
         response = requests.get(url, headers=self.headers)
 
-        if response.status_code == 200:
-            return response.json()
-        else:
+        if response.status_code != 200:
             print("Failed to connect to the API while getting attempt data.")
             raise Exception("Failed to connect to the API while getting attempt data.")
+        
+        return response.json()
 
 
     def get_exercise_data(self, attempt_id):
         url = f"{self.base_url}/pasttest/{attempt_id}"
         response = requests.get(url, headers=self.headers)
-        if response.status_code == 200:
-            return response.json()
-        else:
+
+        if response.status_code != 200:
             print("Failed to connect to the API while getting exercise data.")
             raise Exception("Failed to connect to the API while getting exercise data.")
+        
+        return response.json()
+            
