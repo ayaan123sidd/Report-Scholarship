60894e2b88b1ce91cb7e20d61ab4dfbdc9e11ae0
Ayaan Sidd <developer@academically.com>
2024-06-19 16:41:00 +0530

    subject

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,11 +1,4 @@
-'''
-    SUBJECT DATA FORMAT
-
-    KEY: The name of the subject
-    VALUE: Tuple containing the class_id and the test_id
-'''
 SUBJECT_DATA = {
-    "kaps": (238659, 74556), 
     "medicos": (262859, 89822),
     "adc": (272073, 98161),
     "nclex": (280097, 105398),
@@ -14,12 +7,105 @@ SUBJECT_DATA = {
     "ocanz": (285909, 113240),
     "psi": (286125, 113417),
     "sple": (286126, 113418),
-    "moh":(286797,113417),
-    "amcmock": (292554, 119729),
+    "kaps": {
+        "class_id": 238659,
+        "test_id": 74556,
+        "topics": [
+            {
+                "name": "CVS",
+                "total_questions": "5",
+            },
+            {
+                "name": "ANS",
+                "total_questions": "5",
+            },
+            {
+                "name": "Biopharmaceutics AND Calculations",
+                "total_questions": "5",
+            },
+            {
+                "name": "GIT",
+                "total_questions": "5",
+            },
+            {
+                "name": "CNS",
+                "total_questions": "5",
+            },
+            {
+                "name": "Drug Interaction",
+                "total_questions": "5",
+            },
+            {
+                "name": "Pharmaceutics",
+                "total_questions": "5",
+            },
+            {
+                "name": "ANTIMICROBIALS, ANTI-CANCER, TOXICOLOGY",
+                "total_questions": "5",
+            },
+            {
+                "name": "ENDOCRINOLOGY",
+                "total_questions": "5",
+            },
+            {
+                "name": "CHEMISTRY",
+                "total_questions": "5",
+            }
+        ],
+        "total_questions": 50,
+        "total_marks": 50,
+    },
+    "amcmock": {
+        "class_id": 292554,
+        "test_id": 119729,
+        "topics": [
+            {
+                "name": "CVS",
+                "total_questions": "15"
+            },
+            {
+                "name": "Mental Health",
+                "total_questions": "15"
+            },
+            {
+                "name": "Women Health",
+                "total_questions": "15"
+            },
+            {
+                "name": "Respiratory",
+                "total_questions": "15"
+            },
+            {
+                "name": "Hematology",
+                "total_questions": "15"
+            },
+            {
+                "name": "Dermatology",
+                "total_questions": "15"
+            },
+            {
+                "name": "Urology",
+                "total_questions": "15"
+            },
+            {
+                "name": "Infectious Disease",
+                "total_questions": "15"
+            },
+            {
+                "name": "Breast and Endocrine System",
+                "total_questions": "15"
+            },
+            {
+                "name": "ENT and Ophthalmology",
+                "total_questions": "15"
+            }
+        ],
+        "total_questions": 150,
+        "total_marks": 150,
+
+    }
 }
-SUBJECT = {
-    
-}
+
 LMS_API_HEADERS = {"apiKey": "8209d837743ef9f4b1699ffaa36fe69a", "ORGID": "5735"}
 
 WKHTMLTOPDF_PATH = "/usr/local/bin/wkhtmltopdf"
